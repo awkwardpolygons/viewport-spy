74345485a1a9ea99eccae162d0f87cbc202e8dba
Adrien <adrien@arccoza.com>
2021-04-28 12:35:49 +0800

    Remove spy from inspector

--- addons/viewport-spy/plugin.gd
+++ addons/viewport-spy/plugin.gd
@@ -7,14 +7,14 @@ var inspect_viewport
 var spy
 
 func _enter_tree():
-	inspect_viewport = preload("res://addons/viewport-spy/viewport_inspector.gd").new()
-	add_inspector_plugin(inspect_viewport)
+#	inspect_viewport = preload("res://addons/viewport-spy/viewport_inspector.gd").new()
+#	add_inspector_plugin(inspect_viewport)
 	spy = Spy.new()
 	spy.visible = false
 	add_control_to_container(EditorPlugin.CONTAINER_PROPERTY_EDITOR_BOTTOM, spy)
 
 func _exit_tree():
-	remove_inspector_plugin(inspect_viewport)
+#	remove_inspector_plugin(inspect_viewport)
 	remove_control_from_container(EditorPlugin.CONTAINER_PROPERTY_EDITOR_BOTTOM, spy)
 	if is_instance_valid(spy):
 		spy.queue_free()
