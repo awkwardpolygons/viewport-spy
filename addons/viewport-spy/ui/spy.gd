cc9375fe67d3868744f5351542a638f0d0be0a1d
Adrien <adrien@arccoza.com>
2021-04-28 16:48:52 +0800

    Add spy zoom control and behaviour

--- addons/viewport-spy/ui/spy.gd
+++ addons/viewport-spy/ui/spy.gd
@@ -5,8 +5,10 @@ extends VBoxContainer
 const icon_shown = preload("res://addons/viewport-spy/ui/icons/visible.svg")
 const icon_hidden = preload("res://addons/viewport-spy/ui/icons/hidden.svg")
 const SpyView = preload("res://addons/viewport-spy/ui/spy_view.gd")
+const SpyZoom = preload("res://addons/viewport-spy/ui/spy_zoom.gd")
 var object: Viewport setget set_object
 var view: SpyView
+var zoom: SpyZoom
 var toggle: Button
 
 func set_object(v):
@@ -25,6 +27,9 @@ func _init():
 	view.rect_min_size.y = 256.0
 	view.rect_size = Vector2(256, 256)
 	view.visible = false
+	view.focus_mode = Control.FOCUS_ALL
+	view.connect("gui_input", self, "_on_toggle_zoom")
+	zoom = SpyZoom.new()
 	toggle = Button.new()
 	toggle.toggle_mode = true
 	toggle.text = "Preview"
@@ -34,6 +39,7 @@ func _init():
 
 func _enter_tree():
 	add_child(view)
+	add_child(zoom)
 	add_child(toggle)
 	
 	set_object(object)
@@ -41,12 +47,21 @@ func _enter_tree():
 func _exit_tree():
 	if is_instance_valid(view):
 		view.texture = null
+		view.disconnect("gui_input", self, "_on_toggle_zoom")
 		view.queue_free()
+	if is_instance_valid(zoom):
+		zoom.texture = null
+		zoom.queue_free()
 	if is_instance_valid(toggle):
-		toggle.queue_free()
 		toggle.disconnect("toggled", self, "_on_toggle_view")
+		toggle.queue_free()
 
 func _on_toggle_view(visible):
 	view.visible = visible
 	object.set_meta("__spy", visible)
 	toggle.icon = icon_shown if visible else icon_hidden
+
+func _on_toggle_zoom(event):
+	if event is InputEventMouseButton and event.pressed:
+		zoom.texture = object.get_texture()
+		zoom.popup_centered_ratio(0.6)
